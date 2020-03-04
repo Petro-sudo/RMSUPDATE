61f73db6f55cd93e1a3810a5be2f04485b5c628a
Petro-sudo <petro.malele@gmail.com>
2020-03-04 15:59:28 +0200

    first commit

--- src/app/soshanguve/soshanguve.page.ts
+++ src/app/soshanguve/soshanguve.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {ServiceService} from './../service.service';
 
 @Component({
   selector: 'app-soshanguve',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./soshanguve.page.scss'],
 })
 export class SoshanguvePage implements OnInit {
-
-  constructor() { }
-
+ 
+constructor(private _serviceService: ServiceService) { }
+addProp: any=[];
+addimg: any=[];
   ngOnInit() {
+    this.getProperty();
+   this.img();
   }
+
+  getProperty(){
+   return  this._serviceService.getApartment().
+   subscribe((apart:any)=>
+    {this.addProp = apart;
+      console.log(this.addProp);
+    });
+  }
+
+
+
+  img(){
+    return  this._serviceService.getApartment().
+    subscribe((img:any)=>
+     {this.addProp = img;
+       console.log(this.addimg);
+     });
+   }
+
 
 }
