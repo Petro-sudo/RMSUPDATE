bb9aa5c8a54547541b53747c74f08cbc2ec1608e
Petro-sudo <petro.malele@gmail.com>
2020-03-19 22:07:07 +0200

    lordeditprofile

--- src/app/editlordprof/editlordprof.page.ts
+++ src/app/editlordprof/editlordprof.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-editlordprof',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditlordprofPage implements OnInit {
 
-  constructor() { }
+  edit: any = [];
+  data: Observable<any>;
+ 
+   constructor(public navCtrl: NavController, public http: HttpClient, private _serviceService : ServiceService) { }
+ 
+   ngOnInit() {
+ 
+     this.getData();
+   }
+ 
+   getData(){
+ 
+ 
+    this. _serviceService.lordprofile().subscribe(data=>{
+     this.edit = data;
+     });
+     
+   }
 
-  ngOnInit() {
-  }
-
+  //  myFunction() {
+  //   var x = document.getElementById("myInput");
+  //   if (x.type === "password") {
+  //       x.type = "text";
+  //   } else {
+  //       x.type = "password";
+  //   }
+  //   }
 }
+
