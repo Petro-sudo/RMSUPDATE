19a4ce7e231501dcad7a52f3a2a2d3b5aae69aba
cecillaka <ceciltebogolaka@gmail.com>
2020-04-17 17:09:42 +0200

    landlord menu update

--- src/app/landlord-dash/landlord-dash.page.ts
+++ src/app/landlord-dash/landlord-dash.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, MenuController, LoadingController, ActionSheetController } from '@ionic/angular';
+import { NavController, MenuController, LoadingController, ActionSheetController, Platform } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ServiceService } from '../service.service';
 import { Router, RouterEvent } from '@angular/router';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-landlord-dash',
@@ -14,72 +16,85 @@ export class LandlordDashPage implements OnInit {
 
   result: any = [];
   data: Observable<any>;
+ 
+  navigate: { title: string; url: string; icon: string; }[];
 
   
 
  
    constructor(public navCtrl: NavController, public http: HttpClient, private _serviceService : ServiceService,
     public menuCtrl: MenuController, private router: Router, public actionSheetCtrl: ActionSheetController,
-    private loadingCtrl: LoadingController) {}
+    private loadingCtrl: LoadingController,
+    
+    
+    private platform  : Platform,
+              private splashScreen: SplashScreen,
+              private statusBar   : StatusBar
+    
+    
+    ) {}
 
      
 
    addProp: any=[];
    addimg: any=[];
 
-   async presentMenu() {
-     const actionSheet = await this.actionSheetCtrl.create({
-       header:'Land-Lord Menu',
-       buttons:[{
-         text: 'Profile',
-         role: 'destrustctive',
-         icon: 'person-circle-outline',
-         handler:()=>{
+  //  async presentMenu() {
+  //    const actionSheet = await this.actionSheetCtrl.create({
+  //      header:'Land-Lord Menu',
+  //      buttons:[{
+  //        text: 'Profile',
+  //        role: 'destrustctive',
+  //        icon: 'person-circle-outline',
+  //        handler:()=>{
            
-           this.router.navigateByUrl('landlord-dash');
-         }
-       },{
-         text: 'Register Resident',
-         icon: 'add',
-         handler: () =>{
-         this.router.navigateByUrl('/landloard-app'); 
-         }
-       },{
-       text: 'Manage Student',
-       icon: 'people',
-       handler: () =>{
-         this.router.navigateByUrl('managestud');
-       }
-       },
-       {
-         text: 'Manage Properties',
-         icon: 'business',
-         handler: () =>{
-           this.router.navigateByUrl('manageprop');
-         }
-       },
-       {
-         text:'Edit Your Profile',
-         icon:'person-add',
-         handler: () =>{
-           this.router.navigateByUrl('editlordprof');
-         }
-       },
-       {
-         text: 'Logout ',
-         icon: 'log-out',
-         handler: () =>{
-           this.router.navigateByUrl('/folder/folder');
-         }
-       }
-      ]
-     });
-     await actionSheet.present();
-   }
+  //          this.router.navigateByUrl('landlord-dash');
+  //        }
+  //      },{
+  //        text: 'Register Resident',
+  //        icon: 'add',
+  //        handler: () =>{
+  //        this.router.navigateByUrl('/landloard-app'); 
+  //        }
+  //      },{
+  //      text: 'Manage Student',
+  //      icon: 'people',
+  //      handler: () =>{
+  //        this.router.navigateByUrl('managestud');
+  //      }
+  //      },
+  //      {
+  //        text: 'Manage Properties',
+  //        icon: 'business',
+  //        handler: () =>{
+  //          this.router.navigateByUrl('manageprop');
+  //        }
+  //      },
+  //      {
+  //        text:'Edit Your Profile',
+  //        icon:'person-add',
+  //        handler: () =>{
+  //          this.router.navigateByUrl('editlordprof');
+  //        }
+  //      },
+  //      {
+  //        text: 'Logout ',
+  //        icon: 'log-out',
+  //        handler: () =>{
+  //          this.router.navigateByUrl('/folder/folder');
+  //        }
+  //      }
+  //     ]
+  //    });
+  //    await actionSheet.present();
+  //  }
    ngOnInit() {
  
      this.getData();
      this.applidstu();
+     
+    this.sideMenu();
+    this.initializeApp();
    }
  
 
@@ -107,6 +122,97 @@ export class LandlordDashPage implements OnInit {
   //  toggleLeftMenu() {
   //   this.menuCtrl.toggle();
   // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  sideMenu()
+  {
+    this.navigate =
+    [
+      {
+        title : "HOME",
+        url   : "/home",
+        icon  : "home"
+      },
+      {
+        title : "RESIDENCES",
+        url   : "#properties",
+        icon  : "chatboxes"
+      },
+      {
+        title : "WHO WE ARE",
+        url   : "/contacts",
+        icon  : "contacts"
+      },
+
+
+      {
+        title : "TALK TO US",
+        url   : "/contacts",
+        icon  : "contacts"
+      },
+    ]
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 
    
