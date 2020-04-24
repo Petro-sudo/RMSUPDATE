62d91891ec316bf09259538db2df9aed54a891b6
Petro-sudo <petro.malele@gmail.com>
2020-04-24 14:44:14 +0200

    editprop

--- src/app/manageprop/editprop/editprop.page.ts
+++ src/app/manageprop/editprop/editprop.page.ts
@@ -22,6 +22,7 @@ export class EditpropPage implements OnInit {
     }
   ngOnInit() {
   }
+  
   url1="/assets/pics/kit.jpg"
   onFileSelected(event){
     if(event.target.files){
@@ -34,5 +35,44 @@ export class EditpropPage implements OnInit {
 
  //  this.selectedFile =  <File>event.target.files[0];
  }
+ url2="/assets/pics/bed.jpg"
+ onBed(event){
+   if(event.target.files){
+     var reader =new FileReader();
+     reader.readAsDataURL(event.target.files[0]);
+     reader.onload=(event:any)=>{
+       this.url2=event.target.result;
+     }
+   }
+
+//  this.selectedFile =  <File>event.target.files[0];
+}
+
+url3="/assets/pics/bath.jpg"
+ onBath(event){
+   if(event.target.files){
+     var reader =new FileReader();
+     reader.readAsDataURL(event.target.files[0]);
+     reader.onload=(event:any)=>{
+       this.url3=event.target.result;
+     }
+   }
+
+//  this.selectedFile =  <File>event.target.files[0];
+}
+
+url4="/assets/pics/study.jpg"
+ onStudy(event){
+   if(event.target.files){
+     var reader =new FileReader();
+     reader.readAsDataURL(event.target.files[0]);
+     reader.onload=(event:any)=>{
+       this.url4=event.target.result;
+     }
+   }
+
+//  this.selectedFile =  <File>event.target.files[0];
+}
+
 
 }
