ebb64d1ff6508948b2f5072b30f312fa545f5bfc
Petro-sudo <petro.malele@gmail.com>
2020-04-27 04:58:28 +0200

    new

--- src/app/resproof/resproof.page.ts
+++ src/app/resproof/resproof.page.ts
@@ -1,9 +1,11 @@
 
 import { Component, OnInit} from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, Platform } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {ServiceService} from './../service.service';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 
 @Component({
@@ -15,12 +17,60 @@ export class ResproofPage implements OnInit{
   result: any = [];
  data: Observable<any>;
 
-  constructor(public navCtrl: NavController, public http: HttpClient, private _serviceService : ServiceService) { }
-
+  constructor(public navCtrl: NavController, public http: HttpClient, private _serviceService : ServiceService,
+    private platform    : Platform,
+    private splashScreen: SplashScreen,
+    private statusBar   : StatusBar) { }
+    navigate : any;
   ngOnInit() {
 
     this.getData();
-  }
+    this.initializeApp();
+    this. sideMenu();
+      }
+    
+    
+    
+      initializeApp() {
+        this.platform.ready().then(() => {
+          this.statusBar.styleDefault();
+          this.splashScreen.hide();
+        });
+      }
+    
+      sideMenu()
+      {
+        this.navigate =
+        [
+          {
+            title : "Profile",
+            url   : "/stud-profile",
+            icon :"person-outline"
+           
+           
+          },
+          {
+            title : "Overview",
+            url   : "/resproof",
+            icon  :"eye-outline"
+           
+          },
+          {
+            title : "My rooms",
+            url   : "/studstatus",
+            icon  : "bed-outline"
+          },
+          {
+            title : "Issues",
+            url   : "/stud-app",
+            icon  : "add"
+          },
+    
+    
+          
+        ]
+      }
+    
 
   getData(){
 
