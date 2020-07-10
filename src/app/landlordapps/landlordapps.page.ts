b834a3a7ce9775fdd1be5d42852a83dfa2857606
cecillaka <ceciltebogolaka@gmail.com>
2020-07-11 00:46:41 +0200

    landlord application

--- src/app/landlordapps/landlordapps.page.ts
+++ src/app/landlordapps/landlordapps.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+/*import {NavController} from '@ionic/angular';*/
+import {Observable} from 'rxjs';
+import {ServiceService} from './../service.service';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { Platform, AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import {  ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-landlordapps',
@@ -7,9 +17,298 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandlordappsPage implements OnInit {
 
-  constructor() { }
+  constructor(
 
+    private platform    : Platform,
+    private _serviceService: ServiceService,
+        private splashScreen: SplashScreen,
+        private statusBar   : StatusBar,
+        public alertCtrl: AlertController,
+        private router: Router,
+        public alertController: AlertController
+  ) { }
+
+
+  public addProp: any=[];
+  public addProps: any=[];
+  addimg: any=[];
+  public searchTerm: string = "";
+  public items: any;
+  navigate : any;
   ngOnInit() {
+
+    this.getProperty();
+    this.initializeApp();
+    this.sideMenu();
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+
+propinfo(){
+  this.router.navigate(['/propadmin']);
+
+}
+
+
+
+
+async AlertPrompt() {
+  const alert = await this.alertController.create({
+    header: 'Are you sure You want to Decline this Application?',
+    // message: 'I cannot login',
+
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Confirm Cancel');
+        }
+      }, {
+        text: 'Confirm',
+        handler: () => {
+          console.log('Confirm Submit');
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async presentAlertPrompt() {
+  const alert = await this.alertController.create({
+    header: 'Are you sure You want to Accept this Application?',
+    // message: 'I cannot login',
+
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Confirm Cancel');
+        }
+      }, {
+        text: 'Confirm',
+        handler: () => {
+          console.log('Confirm Submit');
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+async edit() {
+  const alert = await this.alertController.create({
+    header: 'Edit Landlord info',
+    // message: 'I cannot login',
+    inputs: [
+      {
+        name: 'name1',
+        type: 'number',
+        placeholder: 'Number of rooms'
+      },
+      {
+        name: 'name2',
+        type: 'text',
+        placeholder: 'Property Name'
+      },
+    
+      {
+        name: 'name3',
+        type: 'text',
+        placeholder: 'Owner name'
+      },
+      {
+        name: 'name5',
+        type: 'text',
+        placeholder: 'Street Address'
+      },
+      // multiline input.
+      {
+        name: 'paragraph',
+        id: 'paragraph',
+        type: 'textarea',
+        placeholder: 'Property Name'
+      },
+     
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Confirm Cancel');
+        }
+      }, {
+        text: 'confirm',
+        handler: () => {
+          console.log('Confirm Submit');
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
+
+
+
+
+
+
+
+
+
+  sideMenu()
+  {
+    this.navigate =
+    [
+      {
+        title : "Dashboard",
+        url   : "/admindash",
+        icon :"person-outline"
+       
+       
+      },
+    
+      {
+        title : "Manage Students",
+        url   : "/managestud",
+        icon  : "book"
+      },
+      {
+        title : " Landlord Application",
+        url   : "/landlordapps",
+        icon  : "key"
+      },
+
+      {
+        title : " Manage approved properties",
+        url   : "/propinfo",
+        icon  : "home"
+      },
+      
+    ]
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  getProperty(){
+    var searchTerm= "";
+     return  this._serviceService.getApartment().
+     subscribe((apart:any)=>
+      {this.addProp = apart;
+        console.log(this.addProp
+          );
+        //  this.filterItems(searchTerm); 
+      });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
